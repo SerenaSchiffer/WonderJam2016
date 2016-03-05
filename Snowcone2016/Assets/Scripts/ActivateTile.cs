d55d7fdad4f7f7613484c22fc7ebd44a3ed39127
Pierre-André Savard <pandre711@gmail.com>
2016-03-04 20:09:30 -0500

    puzzle de tile actiavalbe fonctionnel

--- Snowcone2016/Assets/Scripts/ActivateTile.cs
+++ Snowcone2016/Assets/Scripts/ActivateTile.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class ActivateTile : MonoBehaviour {
 
+    public bool isActivate;
+    private SpriteRenderer tileSprite;
+    OpenGate gateToOpen;
 	// Use this for initialization
 	void Start () {
-	
+        gateToOpen = gameObject.transform.GetComponentInParent<OpenGate>();
+        isActivate = false;
+        tileSprite = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "character")
+        {
+            Debug.Log("Enter");
+            isActivate = !isActivate;
+            if(isActivate)
+            {
+                tileSprite.color = Color.red;
+                gateToOpen.VerifyPuzzle();
+            }
+            else
+            {
+                tileSprite.color = Color.blue;
+            }
+
+        }
+    }
 }
