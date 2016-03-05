5f840facc0f2861b741809c3f6e465aed69607aa
Emile Fortin <sylfschiffer@hotmail.com>
2016-03-04 22:21:01 -0500

    Smoke de rajouté + canon

---
+++ Snowcone2016/Assets/Scripts/Canon.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Canon : MonoBehaviour {
+
+	public GameObject proj = null;
+	public GameObject vase = null;
+
+
+	Vector2 left;
+	Vector2 right;
+	Vector2 target;
+
+	// Use this for initialization
+	void Start () {
+		left = transform.parent.GetChild (1).position;
+		right = transform.parent.GetChild (2).position;
+		target = right;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "LeftSide")
+			target = right;
+		else if (other.gameObject.name == "RightSide")
+			target = left;
+	}
+		
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector2.MoveTowards (transform.position, target, 0.05f);
+	}
+
+
+	public void Fire()
+	{
+		GameObject projec = Instantiate (proj, new Vector2(transform.position.x + 0.5f, transform.position.y + 0.25f), Quaternion.identity) as GameObject;
+		projec.GetComponent<CanonProjectile> ().target = vase;
+	}
+}
