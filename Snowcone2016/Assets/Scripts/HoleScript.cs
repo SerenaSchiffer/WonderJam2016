4bc6a605228811815e32fa59a4fb4bf559a18bf0
Emile Fortin <sylfschiffer@hotmail.com>
2016-03-04 19:37:16 -0500

    block poussable + tentative de trou

---
+++ Snowcone2016/Assets/Scripts/HoleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy (other.gameObject);
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Destroy (other.gameObject);
+	}
+}
