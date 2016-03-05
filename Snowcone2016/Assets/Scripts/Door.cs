591c7b2b7c2bbbf26d7609eab6373abfce815074
Pierre-André Savard <pandre711@gmail.com>
2016-03-05 15:11:02 -0500

    le script des portes fonctionne

---
+++ Snowcone2016/Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OpenDoor()
+    {
+        Destroy(gameObject.transform.GetChild(2).gameObject);
+        Destroy(gameObject.transform.GetChild(3).gameObject);
+    }
+}
