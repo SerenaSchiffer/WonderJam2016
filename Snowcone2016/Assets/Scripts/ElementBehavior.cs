c67ca633e3292604aa25d3e66885294b28be68b1
Nicholas Fillion Lalancette <nicholala74@hotmail.com>
2016-03-04 22:16:35 -0500

    Avancement des element Behavior

--- Snowcone2016/Assets/Scripts/ElementBehavior.cs
+++ Snowcone2016/Assets/Scripts/ElementBehavior.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class ElementBehavior : MonoBehaviour {
 
     public Wires WirePermitted;
-
+    private GameObject character;
 	// Use this for initialization
 	void Start () {
+        character = (GameObject)GameObject.FindGameObjectWithTag("character");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if(character.GetComponent<WireStats>().currentWire == WirePermitted && gameObject.GetComponent<PolygonCollider2D>().isTrigger == false)
+        {
+
+        }
+        else if(gameObject.GetComponent<PolygonCollider2D>().isTrigger == true)
+        {
+
+        }
 	}
 }
