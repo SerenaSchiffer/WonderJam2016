011ef6fee2a5461f35a68b4d7615f72b18714c56
Nicholas Fillion Lalancette <nicholala74@hotmail.com>
2016-03-05 19:06:40 -0500

    Camera center on character at start, CheckPointPrefab, Mecanism of death and Checkpoint at Working, Element Behavior let you pass through it, but make you drop your wire if it's not the right one.

--- Snowcone2016/Assets/Scripts/CameraZoom.cs
+++ Snowcone2016/Assets/Scripts/CameraZoom.cs
@@ -11,6 +11,7 @@ public class CameraZoom : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Camera.main.orthographicSize = cameraSize;
+        
 	}
 	
 	// Update is called once per frame
@@ -18,5 +19,10 @@ public class CameraZoom : MonoBehaviour {
         cameraSize += Input.GetAxis("Mouse ScrollWheel")*-1 * zoomSpeed;
         cameraSize = Mathf.Clamp(cameraSize, minFOV, maxFOV);
         Camera.main.orthographicSize = cameraSize;
+    }
+
+    public void centerCameraOnCharacter()
+    {
+        Camera.main.transform.position = new Vector3(GameObject.FindGameObjectWithTag("character").transform.position.x, GameObject.FindGameObjectWithTag("character").transform.position.z, -10);
     }
 }
