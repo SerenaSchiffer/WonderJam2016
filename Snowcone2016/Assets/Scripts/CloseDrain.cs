a27054ad3d38a2ffd7b0820876c7b74ff3ff4a58
Nicholas Fillion Lalancette <nicholala74@hotmail.com>
2016-03-05 20:37:52 -0500

    Push pour PA

--- Snowcone2016/Assets/Scripts/CloseDrain.cs
+++ Snowcone2016/Assets/Scripts/CloseDrain.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class CloseDrain : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	    //Close drain and destroy water
-	}
+    GameObject water;
+    SpriteRenderer sr;
+    float transp;
+    // Use this for initialization
+    void Start () {
+        //Close drain and destroy water
+        water = GameObject.FindGameObjectWithTag("water");
+        sr = water.GetComponent<SpriteRenderer>();
+        transp = 1f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+       if(water.transform.localScale.x > 0.2f && water.transform.localScale.y > 0.2f)
+        {
+            transp -= 0.005f;
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, transp);
+        }
 	}
 }
