dc085f43b26aea26cb2e69a7bc4316a142426cfe
sportguitarRC <raf87crosby@hotmail.com>
2016-03-06 05:59:25 -0500

    kek multiple

--- Snowcone2016/Assets/Scripts/UI_Buttons.cs
+++ Snowcone2016/Assets/Scripts/UI_Buttons.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Buttons : MonoBehaviour {
+    public GameObject page1 = null;
+    public GameObject page2 = null;
 	public void StartGame()
 	{
 		SceneManager.LoadScene (6);
@@ -13,4 +15,29 @@ public class UI_Buttons : MonoBehaviour {
 	{
 		Application.Quit ();
 	}
+
+    public void KekHelp()
+    {
+        if(page1 && page2)
+        page1.active = true;
+        page2.active = false;
+    }
+
+    public void KekNext()
+    {
+        page1.active = false;
+        page2.active = true;
+    }
+
+    public void KekPrev()
+    {
+        page1.active = true;
+        page2.active = false;
+    }
+
+    public void KekExit()
+    {
+        page1.active = false;
+        page2.active = false;
+    }
 }
